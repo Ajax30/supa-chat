5dfb5f2a99172988b212f9f1727d46ae94e89415
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-03-22 18:24:47 +0200

    Make the upload button beautiful

--- src/components/Auth/FormCard.vue
+++ src/components/Auth/FormCard.vue
@@ -44,20 +44,82 @@ defineProps({
   font-weight: bold;
 }
 
+.form-field {
+  position: relative;
+  margin-bottom: 15px;
+}
+
+.form-field.bordered {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding-bottom: 9px;
+}
+
 .form-card input[type="text"],
 .form-card input[type="email"],
 .form-card input[type="password"] {
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   padding: 0 6px 3px 6px;
   color: #8b8da9;
-  margin-bottom: 15px;
   font-size: 14px;
+}
+
+.file-upload-btn {
+  max-width: 300px;
+  position: relative;
+  padding-left: 0;
+  color: #6c757d;
+}
+
+.file-upload-btn:focus {
+  border-width: 0 !important;
+  outline: none !important;
+  box-shadow: none;
+}
+
+.file-upload-btn::-webkit-file-upload-button {
+  visibility: hidden;
+}
+
+.file-upload-btn::before {
+  content: "Upload";
+  display: inline-block;
+  background-color: #372379;
+  border-radius: 3px;
+  padding: 9px 10px 9px 34px;
+  outline: none;
+  white-space: nowrap;
+  -webkit-user-select: none;
+  cursor: pointer;
+  color: #fff;
+  line-height: 1;
+}
+
+.file-upload-btn:hover::before {
+  background: #4e3e81;
+}
+
+.file-upload-btn:active::before {
+  background: #4e3e81;
+}
+
+.file-upload-btn::after {
+  content: "\f0ee";
+  font-family: "Font Awesome 7 Free";
+  font-weight: 900;
+  text-decoration: inherit;
+  color: #fff;
+  font-size: 16px;
+  padding-right: 12px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 10px;
 }
 
 .form-card button[type="submit"] {
   line-height: 1;
   outline: none;
-  background: #4e3e81;
+  background: #372379;
   border: 0;
   border-radius: 3px;
   padding: 10px 20px;
@@ -69,6 +131,6 @@ defineProps({
 }
 
 .form-card button[type="submit"]:hover {
-  background: #372379;
+  background: #4e3e81;
 }
 </style>
